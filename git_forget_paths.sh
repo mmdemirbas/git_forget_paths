1c656f28c0f1701a7593210d254e9b4139ad8b22
Muhammed Demirbaş <mmdemirbas@gmail.com>
2018-06-19 11:43:45 +0300

    Add protection & support removing relative paths

--- git_forget_paths.sh
+++ git_forget_paths.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 
+echo "[WARN] Are you sure?"
+echo "[WARN] Ensure that you change the PARAMETERS section per your needs and uncomment necessary lines in the SCRIPT section."
+echo "[WARN] Afterwards, you can comment out this line to run script." && exit 1
+
+
 #######################################################################################
 #                                                                                     #
 #                                 P A R A M E T E R S                                 #
@@ -13,11 +18,34 @@ source_url_or_path=https://github.com/Kotlin/kotlin-koans.git
 # an arbitrary non-existant local path to use as clone target:
 target_local_path=kotlin-koans
 
-# top-level files and/or dirs to keep:
-top_level_files_dirs_to_keep=(src build.gradle README.md)
+# top level files and dirs to keep. to be passed to `git_forget_paths_except` function:
+top_level_paths_to_keep=(
+                         "src"
+                         "test"
+                         ".gitignore"
+                         "build.gradle"
+                         )
+
+# relative paths to remove. to be passed to `git_forget_paths` function:
+relative_paths_to_delete=(
+                          "gradle/wrapper"
+                          "gradlew"
+                          "gradlew.bat"
+                          "test/util"
+                          ".idea/runConfigurations/x.xml"
+                          ".idea/runConfigurations/y.xml"
+                          ".idea/runConfigurations/z.xml"
+                         )
+
+# tag patterns to delete (shell patterns using wildcard)
+tags_to_delete=(
+                "*xxx*"
+                "*yyy*"
+                "*zzz*"
+              )
 
 # target git url to push after removing unnecessary paths:
-target_remote_url=https://github.com/Kotlin/kotlin-koans-trimmed.git
+target_remote_url=https://github.com/Kotlin/kotlin-koans-destructed.git
 
 
 #######################################################################################
@@ -34,6 +62,7 @@ array_contains() {
     for i in $@; do
         if [[ $i == $needle ]]; then
             echo 1
+            return
         fi
     done
     echo 0
@@ -42,7 +71,7 @@ array_contains() {
 # prints file & dir names in the current working except the specified ones.
 # usage: ls_except someDirOrFile anotherDirOrFile ...
 ls_except() {
-    for item in *; do
+    for item in $(ls -A); do
         if [ "$(array_contains "${item}" "$@")" == "0" ]; then
             echo $item
         fi
@@ -52,19 +81,19 @@ ls_except() {
 # Removes the given paths and related history from the current git repo.
 # CAUTION! This may take very long time and rewrites git history!
 git_forget_paths() {
-    : mirror original branches &&
-    git checkout HEAD~0 2>/dev/null &&
-    d=$(printf ' %q' "$@") &&
+    : mirror original branches                       &&
+    git checkout HEAD~0 2>/dev/null                  &&
+    d=$(printf ' %q' "$@")                           &&
     git for-each-ref --shell --format='
       o=%(refname:short) b=${o#origin/} &&
       if test -n "$b" && test "$b" != HEAD; then
         git branch --force --no-track "$b" "$o"
       fi
-    ' refs/remotes/origin/ | sh -e &&
-    git checkout - &&
-    git remote rm origin &&
+    ' refs/remotes/origin/ | sh -e                   &&
+    git checkout -                                   &&
+    git remote rm origin
 
-    : do the filtering &&
+    : do the filtering                               &&
     git filter-branch \
       --index-filter 'git rm --ignore-unmatch --cached -r -- '"$d" \
       --tag-name-filter cat \
@@ -80,7 +109,7 @@ git_forget_paths_except() {
 
 # Performs some house-keeping tasks on the current git repo.
 git_cleanup() {
-    git reflog expire --all && \
+    git reflog expire --all &&
     git gc --aggressive --prune=now
     git reflog expire --all --expire-unreachable=0
     git repack -A -d
@@ -104,22 +133,26 @@ git_push_all_branches_to_origin() {
 #                                                                                     #
 #######################################################################################
 
-git clone "$source_url_or_path" "$target_local_path"           &&
-(
-    cd "$target_local_path"                                    &&
-
-    # if you want to specify files to remove instead of files to keep,
-    # use `git_forget_paths` instead of `git_forget_paths_except`:
-    git_forget_paths_except ${top_level_files_dirs_to_keep[@]} &&
-
-    # optional clean-up phase
-    git_cleanup                                                &&
-
-    # push to new origin
-    git remote add origin "$target_remote_url"                 &&
-    git_push_all_branches_to_origin
-)
-
+#git clone "$source_url_or_path" "$target_local_path"           &&
+#(
+#    cd "$target_local_path"                                    &&
+#
+#    ## if you want to specify files to remove instead of files to keep,
+#    ## use `git_forget_paths` instead of `git_forget_paths_except`:
+#    #git_forget_paths_except ${top_level_paths_to_keep[@]}       &&
+#    #git_forget_paths ${relative_paths_to_delete[@]}            &&
+#
+#    ## optional clean-up phase
+#    #git_cleanup                                                &&
+#
+#    ## remove unnecessary tags
+#    #git tag --list ${tags_to_delete[@]} | xargs git tag -d     &&
+#
+#    ## push to new origin
+#    #git remote add origin "$target_remote_url"                 &&
+#    #git_push_all_branches_to_origin                            &&
+#   :
+#)
 
 #######################################################################################
 #                                                                                     #
