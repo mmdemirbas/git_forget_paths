906dd977e82bfe960aafa9ca521bfd51e6cb8e87
Muhammed Demirbaş <mmdemirbas@gmail.com>
2018-06-25 15:25:16 +0300

    Improve error checks & logging to console

--- git_forget_paths.sh
+++ git_forget_paths.sh
@@ -27,22 +27,17 @@ main() {
     prepare_params "$@"
     confirm_params
 
-    if [ ! -d "$target_path" ]; then
-        # If target path doesn't exist, use `init` to create a repository for the first time
-        init
-    else
-        # If both the target path and the required commit hash information exist,
-        # use `update` to create a branch with the new changes made to the original repository:
-        update
-    fi
+    # If target path doesn't exist, use `init` to create a repository for the first time.
+    # Otherwise, use `update` to apply new changes made to the original repository.
+    [[ ! -d "$target_path" ]] && init || update
 }
 
 prepare_params() {
     # ensure that parameters file specified as an argument
     local params_file=$1
     require_non_empty "$params_file" "<params.sh>"
-    [[ ! -e "$params_file" ]] && echo "Params file was missing: $params_file"            && abort
-    [[ ! -f "$params_file" ]] && echo "Params file was not a regular file: $params_file" && abort
+    [[ ! -e "$params_file" ]] && abort "Params file was missing: $params_file"
+    [[ ! -f "$params_file" ]] && abort "Params file was not a regular file: $params_file"
 
     # include the parameters file which defines the required variables
     . "$params_file"
@@ -58,19 +53,19 @@ prepare_params() {
     # ensure source_branch defined and not empty, or default to master.
     source_branch="${source_branch:-master}"
 
-    [[ -f "$target_path" ]] && echo "Target path was a regular file: $target_path" && abort
+    [[ -f "$target_path" ]] && abort "Target path was a regular file: $target_path"
 
     # temporary area to work
     tmp_path="$target_path.tmp"
 }
 
 require_non_empty() {
-    [[ -z "$1" ]] && echo "$2 should be defined and non-empty" && abort
+    [[ -z "$1" ]] && abort "$2 should be defined and non-empty"
 }
 
 abort() {
     echo
-    echo "Aborted. $*" && exit 1
+    echo "[FAILURE] Aborted. $*" && exit 1
 }
 
 success() {
@@ -94,7 +89,7 @@ confirm_params() {
     printf "Then delete also these files:\n"
     printf "  %s\n" "${relative_paths_to_delete[@]}"
     printf "\n"
-    printf "Then delete tags matching these patterns:\n"
+    printf "And delete tags matching these patterns:\n"
     printf "  %s\n" "${tags_to_delete[@]}"
     printf "\n"
     printf "Note that this operation may take too long depending on your repository size.\n"
@@ -108,7 +103,7 @@ confirm_params() {
     echo
 
     # abort unless pressed 'Y' or 'y'
-    [[ $REPLY =~ ^[Yy]$ ]] || abort
+    [[ $REPLY =~ ^[Yy]$ ]] || abort "User cancelled."
 
     echo
 }
@@ -119,20 +114,17 @@ init() {
     local DATE=`date '+%Y-%m-%d_%H-%M-%S'`
     local tmp="$tmp_path/$DATE"
 
-    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$tmp/$source_branch" || abort
-    cd "$tmp/$source_branch" || abort
-    git_unlink_origin
-
-    git_delete_tags
+    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$tmp/$source_branch" || abort "Clone failed: $source_url_or_path"
+    cd "$tmp/$source_branch" || abort "Couldn't cd into $tmp/$source_branch"
     git_truncate_history
 
     # clone to the target at the end
-    git clone "$tmp/$source_branch" "$target_path" || abort
-    cd "$target_path"                              || abort
+    git clone "$tmp/$source_branch" "$target_path" || abort "Clone failed: $tmp/$source_branch"
+    cd "$target_path"                              || abort "Couldn't cd into $target_path"
     git_unlink_origin
 
     # remove temp files
-    echo "Cleanup temporary files"
+    echo "Cleanup temporary files..."
     rm -rf "$tmp"
     rmdir "$tmp_path"
 
@@ -147,24 +139,27 @@ update() {
     local newkeys="$tmp/.newkeys"
     local oldlog="$tmp/.oldlog"
     local oldkeys="$tmp/.oldkeys"
+    local bare="$tmp/bare"
+    local truncated="$tmp/truncated"
+    local recloned="$tmp/truncated-recloned"
 
-    mkdir -p "$tmp"                                                                      || abort
-
-    cd "$target_path"                                                                    || abort
-    git_log_all                > "$newlog"                                               || abort
-    git_log_committer_and_date > "$newkeys"                                              || abort
+    mkdir -p "$tmp"                                                                         || abort "Couldn't create $tmp"
 
     # we need only the remote history first, not the code, hence clone with --bare:
-    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$tmp/bare" --bare || abort
-    cd "$tmp/bare"                                                                       || abort
+    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$bare" --bare      || abort "Clone failed: $source_url_or_path"
+    cd "$bare"                                                                              || abort "Couldn't cd into $bare"
 
-    git_log_all                > "$oldlog"                                               || abort
-    git_log_committer_and_date > "$oldkeys"                                              || abort
+    git_log_all                > "$oldlog"
+    git_log_committer_and_date > "$oldkeys"
+
+    cd "$target_path"                                                                       || abort "Couldn't cd into $target_path"
+    git_log_all                > "$newlog"
+    git_log_committer_and_date > "$newkeys"
 
     # find latest common commit
     common_commit_key=$(grep --max-count=1 -F -f "$newkeys" "$oldkeys")
-    old_commit_line=$(grep "$common_commit_key" "$oldlog")
-    new_commit_line=$(grep  "$common_commit_key" "$newlog")
+    old_commit_line=$(grep --max-count=1 "$common_commit_key" "$oldlog")
+    new_commit_line=$(grep --max-count=1 "$common_commit_key" "$newlog")
     old_commit_hash=$(echo "$old_commit_line" | cut -d" " -f1)
     new_commit_hash=$(echo "$new_commit_line" | cut -d" " -f1)
     commit_date=$(echo "$new_commit_line" | cut -d" " -f2)
@@ -179,59 +174,68 @@ update() {
     echo "  Committer mail : $commit_email"
     echo "  Commit message : $commit_msg"
     echo
+    require_non_empty "$common_commit_key" "No common commit found! common_commit_key"
 
     # now we need the code only after a known commit:
     # todo: ideal solution --shallow-exclude doesn't work. So we use shallow-since
-#    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$tmp/truncated" --shallow-exclude="$old_commit_hash" || abort
-    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$tmp/truncated" --shallow-since="$commit_date" || abort
-    cd "$tmp/truncated"                                                                  || abort
-    git_unlink_origin
-    git_delete_tags
+#    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$truncated" --shallow-exclude="$old_commit_hash" || abort
+    git clone "$source_url_or_path" -b "$source_branch" --single-branch "$truncated" --shallow-since="$commit_date" || abort "Clone failed: $source_url_or_path"
+    cd "$truncated"                                                                           || abort "Couldn't cd into $truncated"
     git_forget_history_including "$old_commit_hash"
     git_truncate_history
 
     # re-cloning generally reduces the repo size
-    git clone "$tmp/truncated" "$tmp/truncated-recloned"                                 || abort
+    git clone "$truncated" "$recloned"                                                        || abort "Clone failed: $truncated"
 
     # Create backup
-    local backup_path="$target_path.bak"
-    echo "Creating backup at $backup_path/$DATE"
-    mkdir -p "$backup_path"                                                              || abort
-    cp -R "$target_path" "$backup_path/$DATE"                                            || abort
+    local backup_parent_path="$target_path.bak"
+    local backup_path="$backup_parent_path/$DATE"
+    echo "Creating backup at \"$backup_path\"..."
+    mkdir -p "$backup_parent_path"                                                            || abort "Couldn't create $backup_parent_path"
+    cp -R "$target_path" "$backup_path"                                                       || abort "Backup failed! Path: $backup_path"
 
     # go back to the graft point of the initial repo and start a branch there
-    echo "Stashing any change in target repository before update"
-    cd "$target_path"                                                                    || abort
-    git stash                                                                            || abort
-
-    if [[ "$source_branch" = "master" ]]; then
-        git checkout -b "master-$DATE"                                                   || abort
-    else
-        # -B: create new or checkout existing
-        git checkout -B "$source_branch"                                                 || abort
-    fi
+    echo "Stashing any change in target repository before update..."
+    cd "$target_path"                                                                         || abort "Couldn't cd into $target_path"
+    git stash                                                                                 || abort "git stash failed!"
 
     # obtain new changes and append to the history of the branch
     remote_name="$DATE"
     remote_branch="$remote_name/$source_branch"
-    git remote add "$remote_name" "$tmp/truncated-recloned"                              || abort
-    git fetch "$remote_name"                                                             || abort
+    git remote add "$remote_name" "$recloned" || abort "Couldn't add remote: $recloned"
+    git fetch "$remote_name"                  || abort "Couldn't fetch remote: $remote_name"
+
+    # create new branch if source_branch is master
+    local target_branch=$([ "$source_branch" = "master" ] && echo "master-$DATE" || echo "$source_branch")
+    # -B: create new or checkout existing
+    git checkout -B "$target_branch" || abort "\`git checkout \"$target_branch\"\` failed!"
+
+    local remote_branch_tail="$(git log "$remote_branch" --format="%H" | tail -1 || abort "Couldn't get git log!")"
+    require_non_empty "$remote_branch_tail" "No new commit! remote_branch_tail"
+
+    # report changes will be made
+    echo
+    echo "New commits:"
+    local log_format="%C(yellow)%H %C(cyan)%cI %C(red)%ce %C(green)%s %C(reset)"
+    git --no-pager log "$remote_name/$source_branch" --pretty="format:  $log_format"
+#    git --no-pager log "$(git log "$remote_branch" --format=format:"%H" | tail -1)" -n 1 --pretty="format:  from tail: $log_format"
+#    git --no-pager log "$(git log "$remote_branch" --format=format:"%H" | head -1)" -n 1 --pretty="format:    to head: $log_format"
+    echo
 
     # usage: git replace [-f] --graft <commit> [<parent>...]
     echo
-    echo "Updating by rewriting history"
-    git reset --hard "$new_commit_hash"                                                  || abort
-    git replace --graft $(git_first_commit_hash "$remote_branch") HEAD                   || abort
-    git reset --hard "$remote_branch"                                                    || abort
-    git_delete_backups
+    echo "Rewriting history..."
+    git replace --graft "$remote_branch_tail" $new_commit_hash || abort "Failed to replace parent of $remote_branch_tail"
+    git reset --hard "$remote_branch"                          || abort "Failed to hard reset to HEAD of $remote_branch!"
     git remote rm "$remote_name"
+    git_delete_backups
 
-    echo "Unstashing changes stashed before update"
+    echo "Unstashing changes stashed before update..."
     git stash pop
 
     # remove temp files
-    echo "Cleanup temporary files"
-    rm -rf "$tmp"
+    echo "Cleanup temporary files... TODO"
+#    rm -rf "$tmp"
     rmdir "$tmp_path"
 
     success
@@ -243,31 +247,34 @@ git_log_all() {
     # %cI: committer date-time
     # %ce: committer e-mail
     # %s: commit message
-    git --no-pager log --pretty=format:'%C(yellow)%H %C(cyan)%cI %C(red)%ce %C(reset)%s' --all || abort
+    git --no-pager log --pretty=format:'%C(yellow)%H %C(cyan)%cI %C(red)%ce %C(reset)%s' --all || abort "Couldn't get git log!"
 }
 
 git_log_committer_and_date() {
     # %cI: committer date-time
     # %ce: committer e-mail
-    git --no-pager log --pretty=format:'%C(cyan)%cI %C(red)%ce' --all || abort
+    git --no-pager log --pretty=format:'%C(cyan)%cI %C(red)%ce' --all || abort "Couldn't get git log!"
 }
 
 # Removes the given paths and related history from the current git repo.
 # CAUTION! This may take very long time and rewrites git history!
-git_truncate_history(){
+git_truncate_history() {
+    git_unlink_origin
+    git_delete_tags
+
     # use a temp file to prevent 'Argument line too long' error
     local tmp=$(realpath ".paths-to-forget.tmp")
-    ls_except "${top_level_paths_to_keep[@]}"  > "$tmp"
-    echo "${relative_paths_to_delete[@]}"     >> "$tmp"
+    ls_except "${top_level_paths_to_keep[@]}" ".git"  > "$tmp"
+    echo "${relative_paths_to_delete[@]}"            >> "$tmp"
 
     echo
-    echo "Files to delete (if exist):"
+    echo "Files to delete (if exist & versioned):"
     echo
     cat "$tmp" | xargs -n 1 printf "  %s\n"
     echo
 
     git filter-branch --index-filter "cat \"$tmp\" | xargs git rm --ignore-unmatch --cached -r --" \
-        --tag-name-filter cat --prune-empty -- --all || abort
+        --tag-name-filter cat --prune-empty -- --all || abort "Couldn't truncate selected paths from the history!"
 
     git_delete_backups
     git_cleanup
@@ -299,23 +306,19 @@ is_excluded_path() {
 }
 
 git_delete_tags(){
-    git tag --list "${tags_to_delete[@]}" | xargs git tag -d  || abort
+    git tag --list "${tags_to_delete[@]}" | xargs git tag -d  || abort "Couldn't delete git tags!"
     git_cleanup
 }
 
 git_cleanup() {
-    git reflog expire --all --expire-unreachable=0
+    # Prune all unreachable objects from the object database
     git gc --aggressive --prune=now
+    git reflog expire --expire-unreachable=0 --verbose --stale-fix --expire=now --all
     git repack -A -d
-    git prune
 }
 
 git_delete_backups() {
     rm -rf .git/refs/original
-}
-
-git_first_commit_hash() {
-    git log "$1" --format=%H | tail -1 || abort
 }
 
 git_unlink_origin() {
